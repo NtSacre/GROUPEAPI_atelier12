15f0d219e2943a5ccd54d546e4e65f42bb94cb25
Juuf09 <aminatadiouf361@gmail.com>
2023-10-03 17:13:03 +0000

    ajout script aminata

--- indext.php
+++ indext.php
@@ -5,3 +5,15 @@ $profesion_s = "Dev Back";
 $parcours_s = "etudiant à Simplon en 1864";
 $ville_Naissance_s = "Dakar";
 echo "Bonjour je m'appelle $nom, j'ai $age ans j'habite à $ville_Naissance et j'exerce le metier $profesion";
+//ajout du script aminata
+$nom_a ="Aminata";
+$age_a = 45;
+$profession_a ="commerçante";
+$ville_de_naissance_a ="Dakar";
+$parcours_a = "Aprés avoir fait une licence en commerce international, aminata a créee son propre entreprise de commerce international";
+echo "<h2>biographie <h2>" ;
+echo "Aminata <br>";
+echo "45 <br>";
+echo "commerçante <br>";
+echo "Dakar <br>";
+echo "Aprés avoir fait une licence en commerce international, aminata a créee son propre entreprise de commerce international". "<br>";
