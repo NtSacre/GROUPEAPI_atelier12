0d6869f661df227a6620e5aaf10974a319fd7b05
khady <bak92147@gmail.comgit config --global user.name khady>
2023-10-03 17:01:32 +0000

    ajout du script index.php khadyba

--- indext.php
+++ indext.php
@@ -1,7 +1,17 @@
+
 <?php
+// exercice 1
 $nom_s = "Ntandou Khadyjaminata";
 $age_s = 24;
 $profesion_s = "Dev Back";
 $parcours_s = "etudiant à Simplon en 1864";
 $ville_Naissance_s = "Dakar";
 echo "Bonjour je m'appelle $nom, j'ai $age ans j'habite à $ville_Naissance et j'exerce le metier $profesion";
+// funtion Biographie
+function Biographie($nom = "Khady Ba", $age = "23 ans", $profession = "Dévéllopeuse Back-end", $villedenaissance = "Dakar", $parcours = "Brevetée et certifiée en tant que technicienne en mécanique générale. Apprenante en développement back-end.") {
+    return "$nom, $age, $profession, $villedenaissance, $parcours";
+}
+
+$biographie = Biographie();
+
+echo $biographie;
