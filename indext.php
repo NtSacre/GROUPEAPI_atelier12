707cffa8599c39a2457af66705ee391e2a3c40d1
Ntandou <sacrentandou2.0@gmail.com>
2023-10-03 16:08:59 +0000

    envoie du fichier index.php

---
+++ indext.php
@@ -0,0 +1,7 @@
+<?php
+$nom_s = "Ntandou Khadyjaminata";
+$age_s = 24;
+$profesion_s = "Dev Back";
+$parcours_s = "etudiant à Simplon en 1864";
+$ville_Naissance_s = "Dakar";
+echo "Bonjour je m'appelle $nom, j'ai $age ans j'habite à $ville_Naissance et j'exerce le metier $profesion";
